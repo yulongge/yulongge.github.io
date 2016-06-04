88570ea76610e06dc56c16b6ee9e2f9dd43197d0
yulongge <geyulong@163.com>
2016-06-04 22:00:57 +0800

    old version

--- js/index.js
+++ js/index.js
@@ -1,9 +1,26 @@
 $(function(){
+	//index
 	$(".in_photo").mouseenter(function(){
 		$(this).addClass("in_p_hover");
 	}).mouseleave(function(){
 		$(this).removeClass("in_p_hover");
 	}).click(function(){
 		window.location.href="list.html";
+	});
+	//list
+	$(".item .con").mouseenter(function(){
+		return;
+		/*$(this).addClass("an_rotateX");
+		var _this = this;
+		setTimeout(function(){
+			//$(_this).hide();
+			$(_this).css("backgroundColor","#fff");
+		},2000);*/
+		$(this)[0].style.webkitTransition='-webkit-transform 3s linear';
+		$(this)[0].style.webkitTransform = "rotateY("+180+"deg)";  
+		$(this).siblings()[0].style.webkitTransition='-webkit-transform 3s linear';
+		$(this).siblings()[0].style.webkitTransform = "rotateY("+180+"deg)";  
+	}).mouseleave(function(){
+		//$(this).removeClass("an_rotateX");
 	});
 });
